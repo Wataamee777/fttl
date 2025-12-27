c60b1e6049b5a7cd416564355d84f8fe43bb2a66
わたあめえ <Wataamee777@gmail.com>
2025-12-27 10:54:45 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import { time, unix, unixMs, tz } from "./time";
 import { info } from "./info";
 import { cooldown } from "./cooldown";
 import { fetchUtil } from "./fetch";
+import { sleep } from "./sleep";
+import { retry } from "./retry";
+import { ms } from "./ms";
+import { formatBytes } from "./format";
+import { random } from "./random";
 
 const fttl = {
   time,
@@ -11,6 +16,11 @@ const fttl = {
   info,
   cooldown,
   fetch: fetchUtil,
+  sleep,
+  retry,
+  ms,
+  formatBytes,
+  random,
 };
 
 export default fttl;
